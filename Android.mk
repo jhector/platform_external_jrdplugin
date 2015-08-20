cbf8727afc54fdbe6183fcadda18d735646016ec
Julian Hector <julian.r.hector@gmail.com>
2015-08-20 15:59:41 -0700

    Automatically install library into correct directory

--- Android.mk
+++ Android.mk
@@ -3,5 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := main.c
 LOCAL_MODULE := libjrd
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_PATH := $(TARGET_OUT)/b2g/plugins
 include $(BUILD_SHARED_LIBRARY)
